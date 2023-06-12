c4cc12cb26df28d36eff643a41372e451d538bfc
Aly <alcamara237@gmail.com>
2023-06-11 21:57:26 -0400

    Added more styling to dynamic user page

--- pages/[id].js
+++ pages/[id].js
@@ -39,7 +39,7 @@ export default function CommentsPage({ tweetData }) {
       <div className="bg-black min-h-screen text-[#E7E9EA] max-w-[1400px] mx-auto flex">
         <Sidebar />
         <div className="sm:ml-16 xl:ml-80 max-w-2xl flex-grow border-gray-700 border-x">
-          <div className="px-3 py-2 text-lg sm:text-xl font-bold border-b border-gray-700 sticky top-0 z-50 flex space-x-2">
+          <div className="px-3 py-2 text-lg sm:text-xl font-bold border-b border-gray-700 sticky top-0 z-50 flex space-x-2 bg-black">
             <Link href={"/"}>
               <ArrowLeftIcon className="w-7 cursor-pointer" />
             </Link>
